28944f5b3aac6074958b4149637d795434a079b4
Dustin Willis Webber <dustin.webber@gmail.com>
2011-06-19 15:13:11 -0400

    highchart code

--- data/public/javascripts/lookycode.js
+++ data/public/javascripts/lookycode.js
@@ -34,6 +34,125 @@ function render(source, data) {
   return template(data);
 }
 
+function buildChart(username, repos) {
+  var chart;
+  var data = { watchers: [], categories: [], forks: [], issues: []};
+  
+  $('#page').prepend('<div id="user-metrics" style="width:960px;margin:auto;"></div>');
+  
+  for (var i=0; i < repos.length; i++) {
+    data.categories.push(repos[i].name)
+    data.watchers.push(repos[i].watchers);
+    data.forks.push(repos[i].forks);
+    data.issues.push(repos[i].open_issues);
+  };
+  
+  $(document).ready(function() {
+     chart = new Highcharts.Chart({
+        chart: {
+           renderTo: 'user-metrics',
+           defaultSeriesType: 'column',
+           backgroundColor: 'transparent',
+           zoomType: 'x',
+           margin: [ 60, 30, null, 60],
+           spacingRight: 20
+        },
+        credits: {
+          enabled: false,
+        },
+        title: {
+          align: 'center',
+          y: 25,
+           text: username + ' repository metrics',
+           style: {
+             color: '#acacac'
+           }
+        },
+        xAxis: {
+          gridLineColor: '#000',
+          categories: data.categories,
+          labels: {
+             rotation: -35,
+             align: 'right',
+             style: {
+                  color: '#adadad',
+                 font: 'normal 0.9em Verdana, sans-serif'
+             }
+          }
+        },
+        yAxis: {
+          gridLineColor: '#515151',
+          opposite: false,
+           title: {
+              text: 'Watchers, Forks, Issues.',
+              margin: 5,
+              style: {
+                fontWeight: 'none',
+                color: '#acacac'
+              }
+           },
+           min: 0
+        },
+        legend: {
+           align: 'right',
+           x: 0,
+           verticalAlign: 'top',
+           y: 7,
+           floating: false,
+           backgroundColor: 'transparent',
+           borderColor: '#000',
+           borderWidth: 0,
+           shadow: false,
+           itemHoverStyle: {
+           	color: '#FFF'
+           },
+           itemStyle: {
+           	cursor: 'pointer',
+           	color: '#d1d1d1'
+           }
+        },
+        plotOptions: {
+          column: {
+            borderColor: 'transparent',
+            borderWidth: 0,
+            color: '#fff' 
+          }
+        },
+        tooltip: {
+          backgroundColor: {
+             linearGradient: [0, 0, 0, 50],
+             stops: [
+                [0, 'rgba(96, 96, 96, .8)'],
+                [1, 'rgba(16, 16, 16, .8)']
+             ]
+          },
+          borderWidth: 0,
+          style: {
+             color: '#FFF'
+          },
+            formatter: function() {
+               return '<b>'+ this.x +'</b>: '+
+                   this.series.name +': '+ this.y +'<br/>';
+            }
+         },
+        series: [{
+           name: 'Watchers',
+           color: '#84c2f0',
+           data: data.watchers
+        },{
+          name: 'Forks',
+          color: '#cb2b00',
+          data: data.forks
+       },{
+          name: 'issues',
+          color: '#79bc79',
+          data: data.issues
+       }]
+     });
+
+  });
+}
+
 Handlebars.registerHelper('repo-description', function(description) {
   if (description != null && description.length > 0) {
     return description;
@@ -136,7 +255,7 @@ GitHub = (function() {
       success: function(data, textStatus, xhr) {
         var repos = data.data;
         $('#page').html("<ul id='repos' class='repos'></ul>");
-
+        
         var count = 1;
         for (var i=0; i < repos.length; i++) {
           if (count == 1) {
@@ -154,20 +273,24 @@ GitHub = (function() {
           };
 
         };
-
+        
         if (self.user) {
           self.user.repos = data.data;
           self.addUserInformation();
         } else {
           self.userError();
         };
-
+        
+        buildChart(self.name, repos);
+        
         $('#page #repos').css({
           opacity: 0
         });
+        
         $('#page #repos').stop().animate({
           'opacity': 1
         }, 500);
+        
       },
       error: function(xhr, textStatus, errorThrown) {
         self.repoError();
@@ -193,21 +316,32 @@ GitHub = (function() {
   
   GitHub.prototype.addUserRepo = function(repo) {
     var self = this;
-    
     var source = " \
       <li class='repo {{klass}}' data-size='{{size}}' data-lang='{{language}}' data-fork='{{fork}}'> \
         <div class='inside'> \
           <div class='repo-title'> \
-            <div class='repo-name'>{{name}}</div> \
+            <div class='repo-name'><a href='{{html_url}}' target='_blank'>{{name}}</a></div> \
             <div class='repo-size'>{{size}} KB</div> \
           </div> \
           <div class='repo-metadata'> \
             <div class='repo-description'>{{truncate description 130 \"This repository description is currently unavailable.\"}}</div> \
           </div> \
           <div class='repo-footer'> \
-            <div class='repo-box first'>{{watchers}}<div class='box-title'>Watchers</div></div> \
-            <div class='repo-box'>{{open_issues}}<div class='box-title'>Open Issues</div></div> \
-            <div class='repo-box last'>{{forks}}<div class='box-title'>Forks</div></div> \
+            <div class='repo-box first'> \
+              <a href='{{html_url}}/watchers' target='_blank'> \
+                {{watchers}}<div class='box-title'>Watchers</div> \
+              </a> \
+            </div> \
+            <div class='repo-box'> \
+              <a href='{{html_url}}/issues' target='_blank'> \
+                {{open_issues}}<div class='box-title'>Open Issues</div> \
+              </a> \
+            </div> \
+            <div class='repo-box last'> \
+              <a href='{{html_url}}/network' target='_blank'> \
+                {{forks}}<div class='box-title'>Forks</div> \
+              </a> \
+            </div> \
           </div> \
         </div> \
       </li>";
@@ -217,8 +351,6 @@ GitHub = (function() {
   
   GitHub.prototype.addUserInformation = function() {
     var self = this;
-    //https://github.com/postmodern/followers
-    //https://github.com/postmodern/following
     var source = " \
     <div id='user-information'> \
       <div class='detail'> \
@@ -242,7 +374,9 @@ GitHub = (function() {
         </li> \
         <li> \
           <div class='user-inside'> \
+            <a href='https://github.com/{{login}}/following' target='_blank'> \
             <div class='info-title blue'>following</div><div class='info-count'>{{following}}</div> \
+            </a> \
           </div> \
         </li> \
         <li> \
@@ -254,7 +388,9 @@ GitHub = (function() {
         </li> \
         <li> \
           <div class='user-inside'> \
-            <div class='info-title red'>public repos</div><div class='info-count'>{{public_repos}}</div> \
+            <a href='{{html_url}}' target='_blank'> \
+              <div class='info-title red'>public repos</div><div class='info-count'>{{public_repos}}</div> \
+            </a> \
           </div> \
         </li> \
       </ul> \
@@ -304,7 +440,7 @@ jQuery(document).ready(function($) {
         userLoader();
         $('#page').attr('style', '') //.html('<div id="loading">Loading...</div>');
         new GitHub(username);
-        $('title').html('Lookycode - ' + username);
+        $('title').html('Looky some code from ' + username);
 
         if (history && history.pushState) {
     			history.pushState(null, document.title, username);
