c5bffcf27d7c0a322c215297c07080019520c9eb
Dustin Willis Webber <dustin.webber@gmail.com>
2011-06-19 23:30:55 -0400

    simplify index route

--- lookycode.rb
+++ lookycode.rb
@@ -7,12 +7,7 @@ VERSION = '0.1.0'
 
 set :root, File.expand_path(File.join(File.dirname(__FILE__)))
 
-get '/' do
-  @username = 'mephux'
-  erb :index
-end
-
-get '/:user' do
+get '/:user?' do
   @username = params[:user] || 'mephux'
   erb :index
 end
